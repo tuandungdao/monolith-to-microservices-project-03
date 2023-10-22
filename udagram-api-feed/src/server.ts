0c8faa47d071dd2c826f4ea8c58c2077dc2e983d
caunhoc98lx <anthonyhien98@gmail.com>
2023-10-22 21:24:18 +0700

    update frontend config

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -22,16 +22,22 @@ import { V0_FEED_MODELS } from "./controllers/v0/model.index";
   // We set the CORS origin to * so that we don't need to
   // worry about the complexities of CORS this lesson. It's
   // something that will be covered in the next course.
-  app.use(cors({
-    allowedHeaders: [
-      'Origin', 'X-Requested-With',
-      'Content-Type', 'Accept',
-      'X-Access-Token', 'Authorization', 'Access-Control-Allow-Origin',
-    ],
-    methods: 'GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    origin: "*",
-  }));
+  app.use(
+    cors({
+      allowedHeaders: [
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "X-Access-Token",
+        "Authorization",
+        "Access-Control-Allow-Origin",
+      ],
+      methods: "GET,HEAD,OPTIONS,PUT,PATCH,POST,DELETE",
+      preflightContinue: true,
+      origin: "*",
+    })
+  );
 
   app.use("/api/v0/", IndexRouter);
 
